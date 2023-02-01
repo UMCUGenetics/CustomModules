33a3989b6332800b6e9d556506455ab7c8abcf19
ellendejong <de.jong.ellen@hotmail.com>
2023-02-01 14:47:33 +0100

    Add filename to error msg.

--- CheckQC/check_qc.py
+++ CheckQC/check_qc.py
@@ -17,7 +17,7 @@ def non_empty_existing_path(file_or_dir):
     if not input_path.is_file() and not input_path.is_dir():
         raise FileNotFoundError(errno_ENOENT, os_strerror(errno_ENOENT), file_or_dir)
     elif not input_path.is_dir() and not input_path.stat().st_size:
-        raise OSError("File is empty.")
+        raise OSError(f"File {file_or_dir} is empty.")
     elif input_path.is_dir() and file_or_dir[::-1][0] != "/":
         return f"{file_or_dir}/"
     return file_or_dir
@@ -53,8 +53,8 @@ def parse_arguments_and_check(args_in):
 def read_yaml(yaml_file):
     yaml_loaded = yaml.safe_load(open(yaml_file))
     if not yaml_loaded:
-        raise ValueError("File is empty.")
-    return yaml_loaded
+        raise ValueError("Could not load YAML.")
+    return yaml.safe_load(open(yaml_file))
 
 
 def check_allowed_operators(qc_operator):
--- CheckQC/test_check_qc.py
+++ CheckQC/test_check_qc.py
@@ -35,7 +35,7 @@ class TestNonEmptyExistingPath():
     def test_empty_file(self, setup_test_path):
         with pytest.raises(OSError) as empty_error:
             check_qc.non_empty_existing_path(setup_test_path + "empty.txt")
-        assert "File is empty." in str(empty_error.value)
+        assert f"File {setup_test_path}empty.txt is empty." in str(empty_error.value)
 
     def test_append_suffix(self, setup_test_path):
         dir_without_suffix = setup_test_path.rstrip("/")
