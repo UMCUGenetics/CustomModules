d22d0789828671399ef70618f11cb5a7298738bd
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2023-06-06 16:26:36 +0200

    Add parse_ped

--- Utils/parse_child_from_fulltrio.py
+++ Utils/parse_child_from_fulltrio.py
@@ -1,6 +1,31 @@
 #! /usr/bin/env python
 import argparse
-from check_kinship import parse_ped
+
+
+def parse_ped(ped_file):
+    samples = {}  # 'sample_id': {'family': 'fam_id', 'parents': ['sample_id', 'sample_id']}
+
+    for line in ped_file:
+        ped_data = line.strip().split()
+        family, sample, father, mother, sex, phenotype = ped_data
+
+        # Create samples
+        if sample not in samples:
+            samples[sample] = {'family': family, 'parents': [], 'children': []}
+        if father != '0' and father not in samples:
+            samples[father] = {'family': family, 'parents': [], 'children': []}
+        if mother != '0' and mother not in samples:
+            samples[mother] = {'family': family, 'parents': [], 'children': []}
+
+        # Save sample relations
+        if father != '0':
+            samples[sample]['parents'].append(father)
+            samples[father]['children'].append(sample)
+        if mother != '0':
+            samples[sample]['parents'].append(mother)
+            samples[mother]['children'].append(sample)
+    return samples
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check kinship output based on ped file.')
