67a5f6eae3917a3b1a28929a10cb3bbf2f2bd815
melferink <m.elferink@umcutrecht.nl>
2024-02-26 10:02:09 +0100

    refactor mark.parametrize

--- GenderCheck/test_calculate_gender.py
+++ GenderCheck/test_calculate_gender.py
@@ -1,5 +1,7 @@
 import calculate_gender
 
+import pytest
+
 
 class MyObject:
     def __init__(self, qual, start, end):
@@ -8,37 +10,37 @@ class MyObject:
         self.reference_end = end
 
 
-def test_is_valid_read():
-    assert calculate_gender.is_valid_read(MyObject(19, True, True), 20) is False
-    assert calculate_gender.is_valid_read(MyObject(20, True, True), 20) is True
-    assert calculate_gender.is_valid_read(MyObject(20, True, True), 19) is True
-    assert calculate_gender.is_valid_read(MyObject(20, False, True), 20) is False
-    assert calculate_gender.is_valid_read(MyObject(20, True, False), 20) is False
+class TestIsValidRead():
+    @pytest.mark.parametrize("read,mapping_qual,expected", [
+        (MyObject(19, True, True), 20, False),  # mapping quality is below the threshold
+        (MyObject(20, True, True), 20, True),  # mapping quality is equal to the threshold
+        (MyObject(20, True, True), 19, True),  # mapping quality is higher than the threshold
+        (MyObject(20, False, True), 20, False),  # reference_end is false
+        (MyObject(20, True, False), 20, False),  # reference_start is false
+    ])
+    def test_is_valid_read(self, read, mapping_qual, expected):
+        assert expected == calculate_gender.is_valid_read(read, mapping_qual)
 
 
-def test_get_gender_from_bam():
-    assert calculate_gender.get_gender_from_bam("./test_bam.bam", 20, "Y:2649520-59034050", 0.02, 0.12) == "male"
-    assert calculate_gender.get_gender_from_bam("./test_bam.bam", 20, "Y:2649520-59034050", 0.22, 0.32) == "female"
-    assert calculate_gender.get_gender_from_bam("./test_bam.bam", 20, "Y:2649520-59034050", 0.02, 0.32) == "unknown"
+class TestGetGenderFromBam():
+    @pytest.mark.parametrize("bam,mapping_qual,locus_y,ratio_y_female,ratio_y_male,expected", [
+        ("./test_bam.bam", 20, "Y:2649520-59034050", 0.02, 0.12, "male"),  # output male
+        ("./test_bam.bam", 20, "Y:2649520-59034050", 0.22, 0.32, "female"),  # output female
+        ("./test_bam.bam", 20, "Y:2649520-59034050", 0.02, 0.32, "unknown"),  # output unknown
+    ])
+    def test_get_gender_from_bam(self, bam, mapping_qual, locus_y, ratio_y_female, ratio_y_male, expected):
+        assert expected == calculate_gender.get_gender_from_bam(bam, mapping_qual, locus_y, ratio_y_female, ratio_y_male)
 
 
-def test_compare_gender():
-    assert (
-        calculate_gender.compare_gender("test_sample", "test_analyse", "male", "male") ==
-        "test_sample\ttest_analyse\tmale\tmale\tPASS\n"
-    )
-    assert (
-        calculate_gender.compare_gender("test_sample", "test_analyse", "male", "female") ==
-        "test_sample\ttest_analyse\tmale\tfemale\tFAIL\n"
-    )
-    assert (
-        calculate_gender.compare_gender("test_sample", "test_analyse", "male", "unknown") ==
-        "test_sample\ttest_analyse\tmale\tunknown\tPASS\n"
-    )
-    assert (
-        calculate_gender.compare_gender("test_sample", "test_analyse", "male", "not_detected") ==
-        "test_sample\ttest_analyse\tmale\tnot_detected\tFAIL\n"
-    )
+class TestCompareGender():
+    @pytest.mark.parametrize("sample_id,analysis_id,test_gender,true_gender,expected", [
+        ("test_sample", "test_analyse", "male", "male", "test_sample\ttest_analyse\tmale\tmale\tPASS\n"),  # test_gender and true_gender identical, PASS
+        ("test_sample", "test_analyse", "male", "female", "test_sample\ttest_analyse\tmale\tfemale\tFAIL\n"),  # test_gender and true_gender not identical , FAIL
+        ("test_sample", "test_analyse", "male", "unknown", "test_sample\ttest_analyse\tmale\tunknown\tPASS\n"),  # true_gender unknown, PASS
+        ("test_sample", "test_analyse", "male", "not_detected", "test_sample\ttest_analyse\tmale\tnot_detected\tFAIL\n"),  # true_gender not_detected, FAIL
+    ])
+    def test_compare_gender(self, sample_id, analysis_id, test_gender, true_gender, expected):
+        assert expected == calculate_gender.compare_gender(sample_id, analysis_id, test_gender, true_gender)
 
 
 def test_write_qc_file(tmp_path):
