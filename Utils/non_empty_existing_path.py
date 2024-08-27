0d7c4d1882f22ce6b0e4345b928f6591e0f87063
ellendejong <de.jong.ellen@hotmail.com>
2024-08-27 08:52:28 +0200

    add docstring to non_empty_existing_path

--- Utils/non_empty_existing_path.py
+++ Utils/non_empty_existing_path.py
@@ -1,11 +1,23 @@
 #! /usr/bin/env python
 # Import statements, alphabetic order of main package.
-from csv import writer
 from errno import ENOENT as errno_ENOENT
 from os import strerror as os_strerror
 from pathlib import Path
 
 def non_empty_existing_path(file_or_dir):
+    """
+    This function checks whether the provided file or dir exists and is not empty.
+
+    Args:
+        file_or_dir (string): Input file or directory
+
+    Raises:
+        FileNotFoundError: If input string file_or_dir is neither a file nor a dir.
+        OSError: If input is not a dir and file is empty.
+
+    Returns:
+        string: Provided input file or directory. If dir, suffix '/' might be added.
+    """
     input_path = Path(file_or_dir)
     if not input_path.is_file() and not input_path.is_dir():
         raise FileNotFoundError(errno_ENOENT, os_strerror(errno_ENOENT), file_or_dir)
