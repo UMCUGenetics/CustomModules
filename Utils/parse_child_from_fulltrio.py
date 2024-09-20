79cd433ccce0ee441d8d7eac3045ade4de5707d3
ellendejong <de.jong.ellen@hotmail.com>
2024-09-20 11:21:40 +0200

    revert parse_ped pytest in utils, will be added to dev via another branch

--- Utils/parse_child_from_fulltrio.py
+++ Utils/parse_child_from_fulltrio.py
@@ -3,25 +3,8 @@ import argparse
 
 
 def parse_ped(ped_file):
-    """
-    Parse ped file to a samples dict where per sample (key) a metadata dict (value) is created
-    with the family ID (string), parents (list) and children (list) as content.
-
-    Args:
-        ped_file (open file): Open file object to a table with sample metadata, including the following values:
-            familyID (string): Unique identifier per family. In other words, s
-                samples from the same family will have the same familyID.
-            sampleID (string): Unique identifier of sample.
-            father (string): reference to another sampleID.
-            mother (string): reference to another sampleID.
-            sex (int): 0 (unknown), 1 (male) or 2 (female)
-            phenotype (int): 0 (unknown), 1 (unaffected) or 2 (affected)
-
-    Returns:
-        dict: Per sample (key) a metadata dict (value) with
-            the family ID (string), parents (list) and children (list).
-    """
     samples = {}  # 'sample_id': {'family': 'fam_id', 'parents': ['sample_id', 'sample_id']}
+
     for line in ped_file:
         ped_data = line.strip().split()
         family, sample, father, mother, sex, phenotype = ped_data
