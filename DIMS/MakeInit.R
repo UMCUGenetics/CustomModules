d66a87cef752f15d3013000354aa14280622e2af
mraves2 <M.L.Raves-2@umcutrecht.nl>
2024-11-01 13:50:24 +0100

    MakeInit refactored in OOP style

--- DIMS/MakeInit.R
+++ DIMS/MakeInit.R
@@ -5,27 +5,59 @@ args <- commandArgs(trailingOnly = TRUE)
 
 sample_sheet <- read.csv(args[1], sep = "\t")
 nr_replicates <- as.numeric(args[2])
+run_name <- args[3]
 
-sample_names <- trimws(as.vector(unlist(sample_sheet[1])))
-nr_sample_groups <- length(sample_names) / nr_replicates
-group_names <- trimws(as.vector(unlist(sample_sheet[2])))
-group_names <- gsub("[^-.[:alnum:]]", "_", group_names)
-group_names_unique <- unique(group_names)
+file_names <- base::trimws(as.vector(unlist(sample_sheet[1])))
+nr_samples <- length(file_names) / nr_replicates
+sample_names <- base::trimws(as.vector(unlist(sample_sheet[2])))
+sample_names <- gsub("[^-.[:alnum:]]", "_", sample_names)
+sample_names_unique <- unique(sample_names)
 
 # generate the replication pattern
-repl_pattern <- c()
-for (sample_group in 1:nr_sample_groups) {
-  tmp <- c()
-  for (repl in nr_replicates:1) {
-    index <- ((sample_group * nr_replicates) - repl) + 1
-    tmp <- c(tmp, sample_names[index])
-  }
-  repl_pattern <- c(repl_pattern, list(tmp))
-}
-
-names(repl_pattern) <- group_names_unique
+repl_pattern <- lapply(1:nr_samples, function(sample) {
+  indices <- ((sample -1) * nr_replicates+ 1):(sample * nr_replicates)
+  file_names[indices]
+})
+names(repl_pattern) <- sample_names_unique
 
 # preview the replication pattern
 print(tail(repl_pattern))
+
+# create object for run with constructor function
+Run <- function(run_name, date, sample_names, nr_replicates) {
+  run_obj <- list(
+    run_name = run_name,
+    date = date,
+    sample_names = sample_names,
+    nr_replicates = nr_replicates
+  )
+  class(run_obj) <- "Run"
+  return(run_obj)
+}
+
+current_run <- Run(run_name = run_name, samples = group_names, nr_replicates = nr_replicates)
+save(current_run, file = "run_info.RData")
+
+Sample <- function(sample_name, technical_replicates, averaged_peaklist_pos, averaged_peaklist_neg) {
+  sample_obj <- list(
+    sample_name = sample_name,
+    technical_replicates = technical_replicates,
+    averaged_peaklist_pos = NULL,
+    averaged_peaklist_neg = NULL
+  )
+  class(sample_obj) <- "Sample"
+  return(sample_obj)
+}
+
+TechnicalReplicate <- function(sammple_name, peaklist_pos, peaklist_neg) {
+  replicate_obj <- list(
+    sample_name = sample_name,
+    peaklist_pos = NULL,
+    peaklist_neg = NULL
+  )
+  class(replicate_obj) <- "TechnicalReplicate"
+  return(replicate_obj)
+}
+
 
 save(repl_pattern, file = "init.RData")
--- DIMS/MakeInit.nf
+++ DIMS/MakeInit.nf
@@ -7,12 +7,14 @@ process MakeInit {
     input:
        path(samplesheet) 
        val(nr_replicates)
+       val(analysis_id)
 
     output:
+       path('run_info.RData')
        path('init.RData')
 
     script:
         """
-        Rscript ${baseDir}/CustomModules/DIMS/MakeInit.R $samplesheet $nr_replicates 
+        Rscript ${baseDir}/CustomModules/DIMS/MakeInit.R $samplesheet $nr_replicates $analysis_id
         """
 }
