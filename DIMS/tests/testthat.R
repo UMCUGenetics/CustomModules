7e0f192c61318042b572bf42acd753fb330b2386
Mia Pras-Raves <M.L.Raves-2@umcutrecht.nl>
2025-04-22 17:11:38 +0200

    improved unit test for DIMS sum_intensities_adducts

--- DIMS/tests/testthat.R
+++ DIMS/tests/testthat.R
@@ -1,8 +1,5 @@
-# Run all unit tests on sum_adducts
+# Run all unit tests
 library(testthat)
-
-# source all functions for running Outrider
-source("../Utils/sum_adducts.R")
 
 testthat::test_file("testthat/test_sum_intensities_adducts.R")
 
