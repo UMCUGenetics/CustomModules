dce64b93bafac1124c8ae30732ab357ab01f47a3
ALuesink <anneluesink@live.nl>
2025-05-23 14:47:29 +0200

    Testing why snapshot doesn't work on GitHub

--- DIMS/tests/testthat.R
+++ DIMS/tests/testthat.R
@@ -7,6 +7,8 @@ local_edition(3)
 Sys.setenv(NOT_CRAN = "true")
 results_tests <- test_dir("tests/testthat", reporter = "summary")
 
+testthat::snapshot_review('check_qc/')
+
 if (any(vapply(results_tests, function(x) any(x$failed > 0), logical(1)))) {
   quit(status = 1)
 }
