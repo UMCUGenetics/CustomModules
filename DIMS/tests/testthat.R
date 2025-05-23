2556bf5e845753ab1bf3866a00d6d2c1578c444b
ALuesink <anneluesink@live.nl>
2025-05-23 14:32:28 +0200

    Fixed issue CRAN error

--- DIMS/tests/testthat.R
+++ DIMS/tests/testthat.R
@@ -4,13 +4,9 @@ library(withr)
 # enable snapshots
 local_edition(3)
 
-# testthat::test_file("tests/testthat/test_check_qc.R")
-# testthat::test_file("tests/testthat/test_collect_sum_adducts.R")
-# testthat::test_file("tests/testthat/test_generate_excel.R")
-# testthat::test_file("tests/testthat/test_sum_intensities_adducts.R")
-
+Sys.setenv(NOT_CRAN = "true")
 results_tests <- test_dir("tests/testthat", reporter = "summary")
 
-if (any(vapply(results_tests, function(x) anyx$failed > 0, logical(1)))) {
+if (any(vapply(results_tests, function(x) any(x$failed > 0), logical(1)))) {
   quit(status = 1)
 }
