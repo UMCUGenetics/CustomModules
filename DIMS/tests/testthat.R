5dd60f1b3ec50ff36c8512ffe6d4586d7c40c141
ALuesink <anneluesink@live.nl>
2025-05-23 14:01:45 +0200

    Fix exit status unit tests

--- DIMS/tests/testthat.R
+++ DIMS/tests/testthat.R
@@ -4,7 +4,13 @@ library(withr)
 # enable snapshots
 local_edition(3)
 
-testthat::test_file("tests/testthat/test_check_qc.R")
-testthat::test_file("tests/testthat/test_collect_sum_adducts.R")
-testthat::test_file("tests/testthat/test_generate_excel.R")
-testthat::test_file("tests/testthat/test_sum_intensities_adducts.R")
+# testthat::test_file("tests/testthat/test_check_qc.R")
+# testthat::test_file("tests/testthat/test_collect_sum_adducts.R")
+# testthat::test_file("tests/testthat/test_generate_excel.R")
+# testthat::test_file("tests/testthat/test_sum_intensities_adducts.R")
+
+results_tests <- test_dir("tests/testthat", reporter = "summary")
+
+if (any(vapply(results_tests, function(x) anyx$failed > 0), logical(1))) {
+  quit(status = 1)
+}
