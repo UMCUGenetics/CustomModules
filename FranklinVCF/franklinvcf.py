b93c2b7b546313e8bc3aa3560b14f74e94391c6a
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2025-08-06 14:54:33 +0200

    Refactor as UV script, add click

--- FranklinVCF/franklinvcf.py
+++ FranklinVCF/franklinvcf.py
@@ -1,41 +1,43 @@
-#! /usr/bin/env python3
+# /// script
+# requires-python = ">=3.1"
+# dependencies = [
+#   "click",
+#   "vcfpy",
+# ]
+# ///
 
-import os
-import sys
-
-from collections import OrderedDict
+import click
 import vcfpy
+
 
 def add_info_header(reader, ID, Number, Type, Description):
     reader.header.add_info_line(
-        vcfpy.OrderedDict([
-            ('ID', ID),
-            ('Number', Number),
-            ('Type', Type),
-            ('Description', Description)
-        ])
+        vcfpy.OrderedDict([("ID", ID), ("Number", Number), ("Type", Type), ("Description", Description)])
     )
     return reader
 
 
-def convert_vcfs(input_path, output_path):
-    with open(input_path, 'r') as input_vcf, open(output_path, 'w') as output_vcf:
-            reader = vcfpy.Reader.from_stream(input_vcf)
+@click.command()
+@click.argument("input_vcf", type=click.File("r"))
+@click.argument("output_vcf", type=click.File("w"))
+def main(input_vcf, output_vcf):
+    reader = vcfpy.Reader.from_stream(input_vcf)
 
-            # Add filterFlag to INFO field
-            reader = add_info_header(reader, 'filterFlag', ".", 'String', "FILTER flags")
+    # Add filterFlag to INFO field
+    reader = add_info_header(reader, "filterFlag", ".", "String", "FILTER flags")
 
-            # Write new header in new output
-            writer = vcfpy.Writer.from_stream(output_vcf, reader.header)
+    # Write new header in new output
+    writer = vcfpy.Writer.from_stream(output_vcf, reader.header)
 
-            # Get sampleID (assuming single sampl VCF!)
-            sample = reader.header.samples.names[0]
+    # Get sampleID (assuming single sample VCF!)
+    sample = reader.header.samples.names[0]
 
-            for record in reader:
-                #ADD FILTER to INFO field
-                record.INFO['filterFlag'] = record.FILTER
+    for record in reader:
+        # ADD FILTER to INFO field
+        record.INFO["filterFlag"] = record.FILTER
 
-                writer.write_record(record)
+        writer.write_record(record)
+
 
 if __name__ == "__main__":
-    convert_vcfs(sys.argv[1], sys.argv[2])
+    main()
