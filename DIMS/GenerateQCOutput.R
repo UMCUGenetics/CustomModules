d0aff6cf2791fa2b9b1d361d321c6800735ebbbe
Mia Pras-Raves <M.L.Raves-2@umcutrecht.nl>
2025-12-09 12:55:18 +0100

    added as.data.frame to avoid error of non-numeric argument

--- DIMS/GenerateQCOutput.R
+++ DIMS/GenerateQCOutput.R
@@ -329,6 +329,7 @@ if (length(sst_colnrs) > 0) {
   sst_list_intensities <- sst_list[, intensity_col_ids]
 }
 for (col_nr in seq_len(ncol(sst_list_intensities))) {
+  sst_list_intensities <- as.data.frame(sst_list_intensities)
   sst_list_intensities[, col_nr] <- as.numeric(sst_list_intensities[, col_nr])
   if (grepl("Zscore", colnames(sst_list_intensities)[col_nr])) {
     sst_list_intensities[, col_nr] <- round(sst_list_intensities[, col_nr], 2)
