191b1236af9555cdb1744e81be5ecb5de82671dd
Anne Luesink <22368429+ALuesink@users.noreply.github.com>
2026-04-30 13:26:44 +0200

    Revert "DIMS v3.4 bug fixes from Develop to main"

---
+++ DIMS/MakeInit.R
@@ -0,0 +1,29 @@
+# define parameters
+args <- commandArgs(trailingOnly = TRUE)
+
+sample_sheet <- read.csv(args[1], sep = "\t")
+nr_replicates <- as.numeric(args[2])
+
+sample_names <- trimws(as.vector(unlist(sample_sheet[1])))
+nr_sample_groups <- length(sample_names) / nr_replicates
+group_names <- trimws(as.vector(unlist(sample_sheet[2])))
+group_names <- gsub("[^-.[:alnum:]]", "_", group_names)
+group_names_unique <- unique(group_names)
+
+# generate the replication pattern
+repl_pattern <- c()
+for (sample_group in 1:nr_sample_groups) {
+  replicates_persample <- c()
+  for (repl in nr_replicates:1) {
+    index <- ((sample_group * nr_replicates) - repl) + 1
+    replicates_persample <- c(replicates_persample, sample_names[index])
+  }
+  repl_pattern <- c(repl_pattern, list(replicates_persample))
+}
+
+names(repl_pattern) <- group_names_unique
+
+# preview the replication pattern
+print(tail(repl_pattern))
+
+save(repl_pattern, file = "init.RData")
