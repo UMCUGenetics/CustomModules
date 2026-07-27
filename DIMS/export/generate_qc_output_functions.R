49506124b4c238a1cf6c990aee14cdf625e12353
Mia Pras-Raves <M.L.Raves-2@umcutrecht.nl>
2026-07-27 11:52:03 +0200

    cleaned up DIMS/export/generate_qc_output_functions.R

--- DIMS/export/generate_qc_output_functions.R
+++ DIMS/export/generate_qc_output_functions.R
@@ -1,32 +1,33 @@
-check_number_of_controls <- function(outlist, min_num_controls, file_name) {
-  #' Check te number of controls for all metabolites and report findings in a txt file
-  #'
-  #' @param outlist: Dataframe with intensities and Z-scores for all samples and controls
-  #' @param min_num_controls: Integer that is the minimum number of controls
-  #' @param file_name: String containing the file name
+# functions for checking the quality of the dataset (internal standards, positive controls and SST sample)
 
-  outlist_under_ctrls <- outlist %>%
+#' Check the number of control samples for each metabolite and report findings in a txt file
+#'
+#' @param peakgroup_list: Dataframe with intensities and Z-scores for all samples (matrix)
+#' @param min_num_controls: Minimum number of controls (integer)
+#' @param file_name: Output file name (string)
+check_number_of_controls <- function(peakgroup_list, min_num_controls, file_name) {
+  peakgroups_below_nr_control <- peakgroup_list %>%
     filter(nr_ctrls < min_num_controls) %>%
     select(HMDB_name, HMDB_code)
-  if (nrow(outlist_under_ctrls) == 0) {
+  if (nrow(peakgroups_below_nr_control) == 0) {
     writeLines(paste0("All metabolites have", min_num_controls, "or more controls."), file_name)
   } else {
     writeLines(paste0("These metabolites have less than", min_num_controls, "controls. \n"), file_name)
-    write.table(outlist_under_ctrls, file_name, append = TRUE, row.names = FALSE, col.names = TRUE, sep = "\t")
+    write.table(peakgroups_below_nr_control, file_name, append = TRUE, row.names = FALSE, col.names = TRUE, sep = "\t")
   }
 }
 
+#' Get the internal standards data
+#'
+#' @param internal_stand_df: dataframe with all data for all the internal standards (matrix)
+#' @param scanmode: positive, negative or summed (string)
+#' @param is_subset_filter: filter/threshold for outlier control removal (float)
+#' @param dims_matrix: matrix used, e.g. Plasma, Research, etc. (string)
+#' @param rundate: date of pipeline run (Date object)
+#' @param project: project name (string)
+#'
+#' @returns internal_stand: dataframe with the intensities of the internal standards for all samples (matrix)
 get_internal_standards <- function(internal_stand_df, scanmode, is_subset_filter, dims_matrix, rundate, project) {
-  #' Get the internal standards data
-  #'
-  #' @param internal_stand_df: dataframe with all data for all the internal standards
-  #' @param scanmode: positive, negative or summed (string)
-  #' @param is_subset_filter: filter/threshold for outlier control removal (float)
-  #' @param dims_matrix: matrix used, e.g. Plasma, Research, etc. (string)
-  #' @param rundate: date of pipeline run (Date object)
-  #' @param project: project name (string)
-  #'
-  #' @returns internal_stand: dataframe with the intensity of the internal standards for all samples
   if (scanmode == "summed") {
     internal_stand <- internal_stand_df[c(names(is_subset_filter), "HMDB_code")]
     internal_stand$HMDB_name <- internal_stand_df$name
@@ -49,6 +50,16 @@ get_internal_standards <- function(internal_stand_df, scanmode, is_subset_filter
   return(internal_stand)
 }
 
+#' Generate and save internal standard plot
+#'
+#' @param plot_data: Dataframe with the data to be plotted (matrix)
+#' @param plot_type: Type of plot (string)
+#' @param plot_title: Title for the plot (string)
+#' @param outdir: Directory where the plot needs to be saved (string)
+#' @param file_name: Name of the file (string)
+#' @param plot_width: Width of the plot (int)
+#' @param plot_height: Height of the plot (int)
+#' @param hline_data: Dataframe with values for the minimal intensity line (matrix)
 save_internal_standard_plot <- function(
     plot_data,
     plot_type,
@@ -58,17 +69,6 @@ save_internal_standard_plot <- function(
     plot_width,
     plot_height,
     hline_data = NULL) {
-  #' Generate and save internal standard plot
-  #'
-  #' @param plot_data: dataframe with the data to be plotted
-  #' @param plot_type: type of plot (string)
-  #' @param plot_title: title for the plot (string)
-  #' @param outdir: directory where the plot needs to be saved (string)
-  #' @param file_name: name of the file (string)
-  #' @param plot_width: width of the plot (int)
-  #' @param plot_height: height of the plot (int)
-  #' @param hline_data: values for the minimal intensity line (dataframe)
-  #'
 
   # Check if plot_data contains data
   if (nrow(plot_data) == 0) {
@@ -121,16 +121,16 @@ save_internal_standard_plot <- function(
   )
 }
 
-get_pos_ctrl_data <- function(outlist, sample_name, hmdb_codes, hmdb_names) {
-  #' Get the positive control data
-  #'
-  #' @param outlist: dataframe with intensities for all samples
-  #' @param sample_name: positive control sample name(s) (string)
-  #' @param hmdb_codes: HMDB codes of the positive control metabolites (vector)
-  #' @param hmdb_names: HMDB names of the positive control metabolites (vector)
-  #'
-  #' @returns: pos_ctrl_data: dataframe with intensities and Z-scores of the positive control metabolites
-  pos_ctrl_data <- outlist[hmdb_codes, c("HMDB_code", "name", sample_name)]
+#' Get the positive control data
+#'
+#' @param peakgroup_list: Dataframe with intensities for all samples (matrix)
+#' @param sample_name: positive control sample name(s) (string)
+#' @param hmdb_codes: HMDB codes of the positive control metabolites (vector)
+#' @param hmdb_names: HMDB names of the positive control metabolites (vector)
+#'
+#' @returns: pos_ctrl_data: Dataframe with intensities and Z-scores of the positive control metabolites (matrix)
+get_pos_ctrl_data <- function(peakgroup_list, sample_name, hmdb_codes, hmdb_names) {
+  pos_ctrl_data <- peakgroup_list[hmdb_codes, c("HMDB_code", "name", sample_name)]
   pos_ctrl_data <- reshape2::melt(pos_ctrl_data, id.vars = c("HMDB_code", "name"))
   colnames(pos_ctrl_data) <- c("HMDB_code", "HMDB_name", "Sample", "Zscore")
   pos_ctrl_data$HMDB_name <- hmdb_names
@@ -140,26 +140,27 @@ get_pos_ctrl_data <- function(outlist, sample_name, hmdb_codes, hmdb_names) {
   return(pos_ctrl_data)
 }
 
+#' Round numbers to a set number of digits for numeric values
+#'
+#' @param df: Dataframe containing numeric values (matrix)
+#' @param digits: Number of digits to round off to (integer)
+#'
+#' @return df: Dataframe with rounded numbers (matrix)
 round_df <- function(df, digits) {
-  #' Round numbers to a set number of digits for numeric values
-  #'
-  #' @param df: Dataframe containing numeric values
-  #' @param digits: Number of digits to round off to (integer)
-  #'
-  #' @return df: Dataframe with rounded numbers
   numeric_columns <- sapply(df, mode) == "numeric"
   df[numeric_columns] <- round(df[numeric_columns], digits)
+ 
   return(df)
 }
 
+#' Get internal standard intensities
+#'
+#' @param is_data: Dataframe with intensities of internal standards (matrix)
+#' @param int_cols: Indices of internal standard columns (vector of integers)
+#' @param is_codes: Internal standard codes (vector of strings)
+#'
+#' @returns: is_intensities: Dataframe with intensities of internal standards (matrix)
 get_is_intensities <- function(is_data, int_cols = NULL, is_codes = NULL) {
-  #' Get internal standard intensities
-  #'
-  #' @param is_data: dataframe with intensities of internal standards
-  #' @param int_cols: default = NULL, if present indices of internal standard columns
-  #' @param is_codes: default = NULL, if present internal standard codes
-  #'
-  #' @returns: is_intensities: dataframe with intensities of internal standards
   if (is.null(is_codes)) {
     is_intensities <- is_data[, int_cols]
   } else {
@@ -168,15 +169,16 @@ get_is_intensities <- function(is_data, int_cols = NULL, is_codes = NULL) {
   }
   is_intensities <- calc_coefficient_of_variation(is_intensities)
   is_intensities <- cbind(IS_name = is_data$HMDB_name, is_intensities)
+
   return(is_intensities)
 }
 
+#' Calculate coefficent of variation (cv) based on standard deviation (sd) and mean
+#'
+#' @param intensity_list: Intensities (matrix)
+#'
+#' @return intensity_list_with_cv: Intensities and cv, mean, sd (matrix)
 calc_coefficient_of_variation <- function(intensity_list) {
-  #' Calculate coefficent of variation (cv) based on standard deviation (sd) and mean
-  #'
-  #' @param intensity_list: Matrix with intensities
-  #'
-  #' @return intensity_list_with_cv: Matrix with intensities and cv, mean, sd
   intensity_list <- as.data.frame(
     apply(intensity_list, 2, function(x) round(as.numeric(x), 0)),
     row.names = rownames(intensity_list)
@@ -190,24 +192,23 @@ calc_coefficient_of_variation <- function(intensity_list) {
     sd = sd_allsamples,
     intensity_list
   )
+
   return(intensity_list_with_cv)
 }
 
+#' Check if all m/z values are present
+#'
+#' @param mzmed_pgrp_ident: All m/z values for a specific scanmode (vector of float)
+#' @param scanmode: Scan mode, positive or negative (string)
+#'
+#' @return results_mz_missing: Either the missing mz values or a message that no values are missing (string)
 check_missing_mz <- function(mzmed_pgrp_ident, scanmode) {
-  #' Check if all m/z values are present
-  #'
-  #' @param mzmed_pgrp_ident: Vector of all m/z values for a specific scanmode
-  #' @param scanmode: String with the scanmode, positive or negative
-  #'
-  #' @return results_mz_missing: String with either the missing mz values or
-  #' message that no values are missing.
-
   # retrieve all unique m/z values in whole numbers and check if all are available
   mzmed_pgrp_ident <- unique(round(mzmed_pgrp_ident, digits = 0))
   # m/z range for a standard run = 70-600
   mz_range <- seq(70, 599, by = 1)
   mz_missing <- setdiff(mz_range, mzmed_pgrp_ident)
-  # check if m/z are missing and make an .txt file with information
+  # check if m/z are missing and make a txt file with information
   mz_missing_group <- cumsum(c(1, diff(mz_missing) != 1))
   if (length(mz_missing_group) > 1) {
     results_mz_missing <- c(paste0("Missing m/z values ", scanmode, " mode"))
@@ -218,28 +219,28 @@ check_missing_mz <- function(mzmed_pgrp_ident, scanmode) {
   return(results_mz_missing)
 }
 
+#' Create a list of all internal standards with intensity below a threshold value
+#'
+#' @param is_selection_subset: Intensities for each internal standard in each sample (matrix)
+#' @param thresholds: Threshold values for a given scan mode and matrix (vector of integers)
+#' @param is_names: Names of internal standards for a given scan mode (vector of strings)
+#' @param scanmode: Scan mode to include in output (string)
+#'
+#' @return is_below_threshold: All samples for which internal standard intensity is below threshold (matrix)
 find_is_below_threshold <- function(is_selection_subset, thresholds, is_names, scanmode) {
-  #' Create a list of all internal standards with intensity below a threshold value
-  #'
-  #' @param is_selection_subset: Matrix with intensities for each internal standard in each sample
-  #' @param thresholds: Threshold values for a given scan mode and matrix
-  #' @param is_names: Array of names of internal standards for a given scan mode
-  #' @param scanmode: string indicating scan mode to include in output
-  #'
-  #' @return is_below_threshold: Matrix listing all samples for which internal standard intensity is below threshold
-
-  # initialize; get the headers of the matrix
-  is_below_threshold <- is_selection_subset[0, ]
   # for every line, check if intensity is below the appropriate threshold
+  below_threshold_index <- c()
   for (line_index in seq_len(nrow(is_selection_subset))) {
     is_selected <- is_selection_subset$HMDB_name[line_index]
     thresh_selected <- thresholds[which(is_names == is_selected)]
     if (is_selection_subset$Intensity[line_index] < thresh_selected) {
-      is_below_threshold <- rbind(is_below_threshold, is_selection_subset[line_index, ])
+      below_threshold_index <- c(below_threshold_index, line_index)
     }
   }
+  is_below_threshold <- is_selection_subset[below_threshold_index, ]
   # add information on scan mode
-  is_below_threshold <- cbind(is_below_threshold, scanmode = rep(scanmode, nrow(is_below_threshold)))
+  is_below_threshold <- cbind(is_below_threshold, scanmode = scanmode)
+ 
   return(is_below_threshold)
 }
 
