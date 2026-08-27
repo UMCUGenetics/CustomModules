29c9bfb7cf366027ff3635c3dc213bfed436ec2c
Mia Pras-Raves <M.L.Raves-2@umcutrecht.nl>
2026-08-27 16:19:22 +0200

    cleaned up DIMS GenerateViolinPlots.R

--- DIMS/GenerateViolinPlots.R
+++ DIMS/GenerateViolinPlots.R
@@ -1,12 +1,11 @@
-# load packages
+# load required packages
 suppressPackageStartupMessages(library("dplyr"))
+suppressPackageStartupMessages(library("gridExtra"))
 library(reshape2)
 library(openxlsx)
 library(ggplot2)
-suppressPackageStartupMessages(library("gridExtra"))
 library(stringr)
 
-options(digits = 16)
 
 # define parameters
 cmd_args <- commandArgs(trailingOnly = TRUE)
@@ -18,11 +17,31 @@ file_ratios_metabolites <- cmd_args[4]
 file_expected_biomarkers_iem <- cmd_args[5]
 file_explanation <- cmd_args[6]
 
+# Initialize
+options(digits = 16)
+iem_variables <- list(
+  top_number_iem_diseases = 5,
+  threshold_iem = 5
+)
+# number of diseases that score highest in dIEM algorithm to plot
+top_number_iem_diseases <- 5
+# probability score cut-off for plotting the top diseases
+threshold_iem <- 5
+nr_plots_perpage <- 20
+zscore_cutoff <- 5
+protocol_name <- "DIMS_PL_DIAG"
+number_of_metabolites <- list(
+  highest = 20,
+  lowest = 10
+)
+
 # load functions
 source(paste0(export_scripts_dir, "generate_violin_plots_functions.R"))
+
 # load dataframe with intensities and Z-scores for all samples
 intensities_zscore_df <- get(load("outlist.RData"))
 rm(outlist)
+
 # read input files
 metabolites_ratios_df <- read.csv(file_ratios_metabolites, sep = ";", stringsAsFactors = FALSE)
 expected_biomarkers_df <- read.csv(file_expected_biomarkers_iem, sep = ";", stringsAsFactors = FALSE)
@@ -32,21 +51,6 @@ expected_biomarkers_df <- expected_biomarkers_df %>%
     HMDB_name = Metabolite
   )
 explanation_violin_plot <- readLines(file_explanation)
-
-# Set global variables
-iem_variables <- list(
-  top_number_iem_diseases = 5,
-  threshold_iem = 5
-)
-top_number_iem_diseases <- 5 # number of diseases that score highest in algorithm to plot
-threshold_iem <- 5 # probability score cut-off for plotting the top diseases
-nr_plots_perpage <- 20 # number of violin plots per page in PDF
-zscore_cutoff <- 5
-protocol_name <- "DIMS_PL_DIAG"
-number_of_metabolites <- list(
-  highest = 20,
-  lowest = 10
-)
 
 control_ids <- get_colnames_by_prefix(intensities_zscore_df, "C")
 patient_ids <- get_colnames_by_prefix(intensities_zscore_df, "P")
@@ -69,7 +73,7 @@ zscore_controls_df <- intensities_zscore_ratios_df %>%
   select(HMDB_code, HMDB_name, any_of(paste0(control_ids, "_Zscore"))) %>%
   rename_with(~ str_remove(.x, "_Zscore"), .cols = contains("_Zscore"))
 
-#### Make violin plots #####
+# Make violin plots
 make_and_save_violin_plot_pdfs(
   zscore_patients_df,
   zscore_controls_df,
@@ -82,12 +86,12 @@ make_and_save_violin_plot_pdfs(
   number_of_metabolites
 )
 
-#### Run the IEM algorithm #########
+# Run the IEM algorithm
 diem_probability_score <- run_diem_algorithm(expected_biomarkers_df, zscore_patients_df, patient_ids)
 
 save_prob_scores_to_excel(diem_probability_score, run_name)
 
-#### Generate dIEM plots #########
+# Generate dIEM plots
 patient_no_iem <- make_and_save_diem_plots(
   diem_probability_score,
   patient_ids,
